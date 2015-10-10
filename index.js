969bcd453c2dad765818046b929e9a89613fcb07
Varun <rapidwein@gmail.com>
2015-10-10 14:20:39 -0400

    socket server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var app = require("express")(),
+var express = require("express"),
+    http = require("http"),
+    io = require("socket.io"),
     port = 3000,
     ipaddr = 'localhost';
+var app = express();
+var webServer = http.createServer(app).listen(port);
+var socketServer = io.listen(webServer);
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(port, ipaddr, function(){
-  console.log("Started");
 });
